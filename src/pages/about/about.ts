874488061e131f68866446b442e97b25f02d84f3
Shashi shalabh <john_ojha@yahoo.com>
2018-06-27 09:41:01 +0530

    shashi

---
+++ src/pages/about/about.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+public base64Image : string;
+
+  constructor(public navCtrl: NavController,private camera: Camera) {
+
+  }
+
+
+  takepicture()
+  {
+  const options: CameraOptions = {
+  quality: 100,
+  destinationType: this.camera.DestinationType.DATA_URL,
+  encodingType: this.camera.EncodingType.JPEG,
+  mediaType: this.camera.MediaType.PICTURE
+}
+
+this.camera.getPicture(options).then((imageData) => {
+ // imageData is either a base64 encoded string or a file URI
+ // If it's base64:
+  this.base64Image = 'data:image/jpeg;base64,' + imageData;
+
+ 
+}, (err) => {
+ // Handle error
+});
+
+}
+
+
+
+}
